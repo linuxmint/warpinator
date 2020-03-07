c4754f31250b72ed408039c2bfc5c82a83d29cdb
Michael Webster <miketwebster@gmail.com>
2020-03-07 17:15:54 -0500

    Add rpc_fallbacks.py (left out of 10aa40b1e215dcbe).

---
+++ src/rpc_fallbacks.py
@@ -0,0 +1,19 @@
+import os
+
+from gi.repository import Gtk
+
+import transfers
+import util
+
+def default_avatar_iterator():
+    theme = Gtk.IconTheme.get_default()
+
+    s, w, h = Gtk.IconSize.lookup(Gtk.IconSize.DND)
+
+    info = theme.lookup_icon_for_scale("avatar-default",
+                                       w, util.get_global_scale_factor(),
+                                       Gtk.IconLookupFlags.FORCE_SVG)
+
+    path = info.get_filename()
+
+    return transfers.load_file_in_chunks(path)
