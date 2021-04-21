0331df4d287561a06dabe30da54a6c23e7018e10
Michael Webster <miketwebster@gmail.com>
2021-04-20 20:46:23 -0400

    Allow bundling of zeroconf.
    This will help prevent any future api breaks, and have
    the latest improvements as they come.
    
    0.27.0 will be the minimum required version for now.

---
+++ install-scripts/download_zeroconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+#Parts:
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Runs protoc with the gRPC plugin to generate messages and gRPC stubs."""
+
+
+
+import sys
+import os
+
+relative_path = os.path.join("libexec", "warpinator", "zeroconf_")
+install_dir = os.path.join(os.environ['MESON_INSTALL_DESTDIR_PREFIX'], relative_path)
+os.makedirs(install_dir, exist_ok=True)
+
+version = "0.29.0"
+
+if os.environ.get('DESTDIR'):
+    print("\n\nDownloading and packaging zeroconf %s in %s\n\n" % (version, relative_path))
+else:
+    print("\n\nDownloading and installing zeroconf %s in %s\n\n" % (version, install_dir))
+
+os.system('curl -s https://raw.githubusercontent.com/jstasiak/python-zeroconf/%s/zeroconf/__init__.py > %s/__init__.py' % (version, install_dir))
+os.system('curl -s https://raw.githubusercontent.com/jstasiak/python-zeroconf/%s/zeroconf/py.typed > %s/py.typed' % (version, install_dir))
--- src/server.py
+++ src/server.py
@@ -4,10 +4,10 @@ import threading
 import gettext
 import logging
 import time
+import pkg_resources
 from concurrent import futures
 
 from gi.repository import GObject, GLib
-from zeroconf import ServiceInfo, Zeroconf, ServiceBrowser
 
 import grpc
 import warp_pb2
@@ -21,6 +21,21 @@ import transfers
 import wrappers
 from ops import ReceiveOp
 from util import TransferDirection, OpStatus, RemoteStatus
+
+if config.bundle_zeroconf:
+    import zeroconf_
+    from zeroconf_ import ServiceInfo, Zeroconf, ServiceBrowser
+else:
+    import zeroconf
+
+    zc_version = pkg_resources.parse_version(zeroconf.__version__)
+    zc_min_version = pkg_resources.parse_version("0.27.0")
+
+    if zc_version < zc_min_version:
+        print("Python3 zeroconf must be >= %s" % zc_min_version)
+        exit(1)
+
+    from zeroconf import ServiceInfo, Zeroconf, ServiceBrowser
 
 _ = gettext.gettext
 
@@ -64,6 +79,11 @@ class Server(threading.Thread, warp_pb2_grpc.WarpServicer, GObject.Object):
         self.start()
 
     def start_zeroconf(self):
+        try:
+            logging.info("Using bundled zeroconf v%s" % zeroconf_.__version__)
+        except:
+            logging.info("Using system zeroconf v%s" % zeroconf.__version__)
+
         self.zeroconf = Zeroconf()
 
         self.service_ident = auth.get_singleton().get_ident()
