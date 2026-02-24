ba7dacfd8873b20192b8f865ea0589826ebd632e
Michael Webster <miketwebster@gmail.com>
2026-02-24 11:40:56 -0500

    Update generated protobuf files to match our bundled versions.

--- src/warp_pb2_grpc.py
+++ src/warp_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import warp_pb2 as warp__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
